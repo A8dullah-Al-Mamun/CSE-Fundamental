679d9a0ee520f6354febf67297bdaf9d5f28f9da
A8dullah-Al-Mamun <a8dullah.al.mamun@gmail.com>
2026-04-22 22:48:46 +0600

    Print the next letter to C  in the alphabet.

---
+++ ICPC AU Community_CONTEST 1/C_Next_Alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int z;
+    char s;
+    scanf("%c", &s);
+    if (s == 'z')
+    {
+        printf("a");
+    }
+    else 
+    {
+        printf("%c", s + 1);
+        
+    }
+    return 0;
+}
