d15370fdab2725bf844890a01a1838e1d895017c
A8dullah-Al-Mamun <a8dullah.al.mamun@gmail.com>
2026-04-22 22:50:05 +0600

    Check if a□b□c=d using +, −, ×

---
+++ ICPC AU Community_CONTEST 1/D_Ali_Baba_and_Puzzles.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define ll long long int
+
+int main()
+{
+    ll a, b, c, d;
+    scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+    if (a * b + c == d)
+    {
+        printf("YES");
+    }
+    else if (a * b - c == d)
+    {
+        printf("YES");
+    }
+    else if (a + b * c == d)
+    {
+        printf("YES");
+    }
+    else if (a + b - c == d)
+    {
+        printf("YES");
+    }
+    else if (a - b * c == d)
+    {
+        printf("YES");
+    }
+    else if (a - b + c == d)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
