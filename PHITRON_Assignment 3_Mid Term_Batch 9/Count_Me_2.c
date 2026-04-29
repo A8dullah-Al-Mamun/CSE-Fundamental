fd6721efef500000691c89660c2bbe7bb6c7a230
A8dullah-Al-Mamun <a8dullah.al.mamun@gmail.com>
2026-04-29 20:32:08 +0600

    Implement constant counter for lowercase strings

---
+++ PHITRON_Assignment 3_Mid Term_Batch 9/Count_Me_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s[100001];
+    scanf("%s", &s);
+    int length_s = strlen(s), count = 0;
+    for (int i = 0; i < length_s; i++)
+    {
+        if (!(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'))
+        {
+            count++;
+        }
+    }
+
+    printf("%d\n", count);
+    return 0;
+}
