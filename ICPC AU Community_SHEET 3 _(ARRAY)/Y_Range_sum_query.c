ae2e18922b400f443b95b25c1a527bfa2948e02f
A8dullah-Al-Mamun <a8dullah.al.mamun@gmail.com>
2026-04-30 21:49:19 +0600

    Answer queries for sum in range.

---
+++ ICPC AU Community_SHEET 3 _(ARRAY)/Y_Range_sum_query.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, q;
+    scanf("%d %d", &n, &q);
+
+    long long a[n + 1], pre[n + 1];
+
+    pre[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        scanf("%lld", &a[i]);
+        pre[i] = pre[i - 1] + a[i];
+    }
+
+    while (q--) {
+        int l, r;
+        scanf("%d %d", &l, &r);
+
+        long long ans = pre[r] - pre[l - 1];
+
+        printf("%lld\n", ans);
+    }
+
+    return 0;
+}
