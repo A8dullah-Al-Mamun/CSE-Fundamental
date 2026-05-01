1b37437cc100991e9d56821a5b517e2ca23d75d8
A8dullah-Al-Mamun <a8dullah.al.mamun@gmail.com>
2026-05-01 11:01:15 +0600

    Check if two people are brothers.

---
+++ ICPC AU Community_SHEET 1_(Data type -Conditons)/L_The_Brothers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+    char f1[100],s1[100],f2[100],s2[100];
+    scanf("%s %s\n%s %s",f1, s1, f2, s2);
+    if(strcmp(s1,s2)==0)
+    {
+         printf("ARE Brothers\n");
+    }
+    else
+    {
+        printf("NOT");
+    }
+    return 0;
+}
