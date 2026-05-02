86a1987405988b720717e5b34ac9855b718f1da3
A8dullah-Al-Mamun <a8dullah.al.mamun@gmail.com>
2026-05-02 11:14:44 +0600

    Check if array is palindrome.

---
+++ ICPC AU Community_SHEET 3 _(ARRAY)/G_Palindrome_Array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int A[N];
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &A[i]);
+    }
+
+    int palindrome = 1;
+
+    for (int i = 0; i < N / 2; i++) {
+        if (A[i] != A[N - 1 - i]) {
+            palindrome = 0;
+            break;
+        }
+    }
+
+    if (palindrome) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+
+    return 0;
+}
