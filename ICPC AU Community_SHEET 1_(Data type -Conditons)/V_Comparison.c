8ba56aa102b7b0c9674ffce8d6222397018e2309
A8dullah-Al-Mamun <a8dullah.al.mamun@gmail.com>
2026-05-04 15:49:26 +0600

    Compare two numbers with >,<,=.

---
+++ ICPC AU Community_SHEET 1_(Data type -Conditons)/V_Comparison.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    char s;
+
+    scanf("%d %c %d", &a, &s, &b);
+
+    if (s == '>' && a > b)
+    {
+        printf("Right");
+    }
+    else if (s == '<' && a < b)
+    {
+        printf("Right");
+    }
+    else if (s == '=' && a == b)
+    {
+        printf("Right");
+    }
+    else
+    {
+        printf("Wrong");
+    }
+
+    return 0;
+}
